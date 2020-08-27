8d9ac35a1dda8dc306e20ebf8375111e36a706cd
Ivan Marin <ivan.marin2525@gmail.com>
2020-08-27 18:28:29 +0200

    Add rounded edges to images

--- caffebarMenu/src/components/image.js
+++ caffebarMenu/src/components/image.js
@@ -43,6 +43,8 @@ const Image = props => (
         width: width,
         height: height,
         position: "absolute",
+        border: "1px solid lightgrey",
+        borderRadius: "10px",
       }
       return <Img imgStyle={style} alt={props.alt} fluid={imageFluids} />
     }}
