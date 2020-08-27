f29d8ed0593171cb75409ce03699ac88fbf49ac9
Ivan Marin <ivan.marin2525@gmail.com>
2020-08-27 18:29:55 +0200

    Optional hidden class for ingredents

--- caffebarMenu/src/components/items.js
+++ caffebarMenu/src/components/items.js
@@ -3,11 +3,14 @@ import React from "react"
 import style from "../styles/table.module.css"
 
 const Items = ({ menuItems }) => {
+  let hidden = ""
+  if (window.screen.availWidth < 1080) hidden = style.hidden
+  console.log("WidtH: " + window.screen.availWidth)
   return (
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+        gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
         gridColumnGap: "2em",
         gridRowGap: "2em",
       }}
@@ -29,24 +32,30 @@ const Items = ({ menuItems }) => {
                 {drink.name}
               </h3>
             </div>
-            <div style={{ width: "80%", margin: "0 auto" }}>
-              <Image width="100%" height="200px" filename={drink.image}></Image>
+            <div
+              style={{ width: "80%", margin: "0 auto", marginBottom: "20px" }}
+            >
+              <Image width="100%" height="150px" filename={drink.image}></Image>
             </div>
-            <div className={style.featureList}>
+            <div className={`${style.featureList} ${hidden}`}>
               <ul id={style.featureStyle}>
                 {drink.ingredients.map(item => {
                   return <li>{item}</li>
                 })}
               </ul>
             </div>
-            <div
-              style={{
-                position: "absolute",
-                bottom: 10,
-                right: 10,
-              }}
-            >
-              <p style={{ marginBottom: "0" }}>{drink.price}</p>
+            <div>
+              <p
+                style={{
+                  textAlign: "right",
+                  position: "absolute",
+                  bottom: "5px",
+                  right: "10px",
+                  margin: "0",
+                }}
+              >
+                {drink.price}
+              </p>
             </div>
           </div>
         )
